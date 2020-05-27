033d97b9b2914d048e26af02bfaf02285afaff99
WinSpartan <shunter2120@hotmail.com>
2020-05-27 08:16:36 -0400

    Changed title headings

--- Portfolio/src/components/introduction/Introduction.js
+++ Portfolio/src/components/introduction/Introduction.js
@@ -4,13 +4,10 @@ import Scroll from '../common/Scroll';
 const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
-      <h1>Hyperspace</h1>
+      <h1>Stephen Rex Hunter</h1>
       <p>
-        Just another fine responsive site template designed by{' '}
-        <a href="http://html5up.net">HTML5 UP</a>
+        Welcome to my Portfolio!
         <br />
-        and released for free under the{' '}
-        <a href="http://html5up.net/license">Creative Commons</a>.
       </p>
       <ul className="actions">
         <li>
